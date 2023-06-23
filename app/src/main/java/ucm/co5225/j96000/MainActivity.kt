baffc489c0e3b4f02ef90840d13553910b76f457
Devrock2847 <93879277+Devrock2847@users.noreply.github.com>
2023-06-23 13:53:47 +0100

    Update before it's lost

--- app/src/main/java/ucm/co5225/j96000/MainActivity.kt
+++ app/src/main/java/ucm/co5225/j96000/MainActivity.kt
@@ -1,23 +1,25 @@
 package ucm.co5225.j96000
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.View
 import android.widget.*
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
+import androidx.core.content.ContextCompat
+import kotlinx.coroutines.*
 import ucm.co5225.j96000.databinding.ActivityMainBinding
 import org.json.JSONObject
 import java.net.URL
 
-class MainActivity : AppCompatActivity() {
-    private var baseCurrencyOne = "GBP"
-    private var baseCurrencyTwo = "USD"
-    private var conversionRate = 0f
-    private var apiKey = "325b56c003ec0e19ce02de94"
+class MainActivity: AppCompatActivity(), View.OnClickListener {
+    private var baseCurrencyOne = "btc"
+    private var baseCurrencyTwo = "eth"
+    private var conversionRateOne = 0f
+    private var conversionRateTwo = 0f
+    //private var apiKey = "325b56c003ec0e19ce02de94"
+    lateinit var buttonConvert : Button
+    lateinit var buttonClear : Button
     private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -25,11 +27,38 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
-        buildSpinner()
+        buildSpinner(view)
+        buttonConvert = binding.convertButton
+        buttonConvert.setOnClickListener(this)
+        buttonClear = binding.clearButton
+        buttonClear.setOnClickListener(this)
+    }
+
+    override fun onClick(view: View?) {
+        when(view?.id){
+            R.id.convertButton->{
+                getApiResult(view)
+            }
+            R.id.clearButton->{
+                disableButton(buttonClear)
+                clearTextView(view)
+                enableButton(buttonClear)
+            }
+        }
+    }
+    fun buildThread() {
+
     }
     fun getApiResult(view: View) {
+        //val conversionRateCheck = conversionRate
+        //val isTrue = false
+        disableButton(buttonConvert)
         val thread = Thread {
-            var API = "https://v6.exchangerate-api.com/v6/$apiKey/pair/$baseCurrencyOne/$baseCurrencyTwo"
+            //var API = "https://v6.exchangerate-api.com/v6/$apiKey/pair/$baseCurrencyOne/$baseCurrencyTwo"
+            var API = "https://api.coingecko.com/api/v3/exchange_rates"
+            //var APIone = "https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=$baseCurrencyOne"
+            //var APItwo = "https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=$baseCurrencyTwo"
+            //var API = "https://apiv2.bitcoinaverage.com/convert/NTc1NzgzNzlkMjcwNGE3NzgwMDUxOTcwOTU0Y2NhMGQ?from=$baseCurrencyOne&to=$baseCurrencyTwo&amount=1"
             if (binding.editTextConversionFrom != null && binding.editTextConversionFrom.text.isNotEmpty() && binding.editTextConversionFrom.text.isNotBlank()) {
                 //This stops the program crashing when no value is selected
                 if (baseCurrencyOne == baseCurrencyTwo) {
@@ -39,46 +68,63 @@ class MainActivity : AppCompatActivity() {
                         Toast.LENGTH_SHORT
                     ).show()
                 } else {
-                    GlobalScope.launch(Dispatchers.IO) {
-                        try {
-                            //attempts to read from the api
-                            val apiResult = URL(API).readText()
-                            val jsonObject = JSONObject(apiResult)
+                    //Originally not async
+                    //GlobalScope.async(Dispatchers.IO) {
+                    try {
+                        //attempts to read from the api
+                        val apiResult = URL(API).readText()
+                        val jsonObject = JSONObject(apiResult)
+                        //var fromCoin = jsonObject.getString()
+                        //pulls the conversion rate from the api call object
+                        conversionRateOne = jsonObject.getJSONObject("rates").getJSONObject("$baseCurrencyOne").getString("value").toFloat()
+                        conversionRateTwo = jsonObject.getJSONObject("rates").getJSONObject("$baseCurrencyTwo").getString("value").toFloat()
+                        var conversionRate = conversionRateOne / conversionRateTwo * 100
+                        Log.d("Main", "$conversionRateOne")
+                        Log.d("Main", "$conversionRateTwo")
+                        Log.d("Main", "$conversionRate")
+                        Log.d("Main", apiResult)
+                        //this Coroutine takes the data from the input boss and multiplies it
+                        //by the conversion rate and displays inside the output box
 
-                            conversionRate = jsonObject.getString("conversion_rate").toFloat()
+                        val text = ((binding.editTextConversionFrom.text.toString().toFloat()) * conversionRate).toString()
 
-                            if (conversionRate != null) {
-                                binding.textView2.text = conversionRate.toString()
-                            }
-                            //Log.d("Main", "$conversionRate")
-                            //Log.d("Main", apiResult)
-                            //this Coroutine takes the data from the input boss and multiplies it
-                            //by the conversion rate and displays inside the output box
-                            withContext(Dispatchers.Main) {
-                                val text = ((binding.editTextConversionFrom.text.toString()
-                                    .toFloat()) * conversionRate).toString()
-                                runOnUiThread {
-                                    binding.editTextConversionTo?.setText(text)
-                                }
-                            }
-                            //errors if there is an issue with the api connection
+                        binding.editTextConversionTo?.setText(text)
+
+                        if (conversionRate != null) { binding.textView2.text = conversionRate.toString() }
+                        //End the API call here
                         } catch (e: Exception) {
                             Log.e("Main", "$e")
                         }
-                    }
                 }
             }
         }
         thread.start()
+        enableButton(buttonConvert)
+
+        //if(conversionRateCheck != conversionRate) {
+        //if (conversionRate != conversionRateCheck) { thread.interrupt() }
+        //thread.start()
     }
-    fun clearTextView(view: View) {
+    private fun clearTextView(view: View) {
         //functionality for the clear button
         binding.editTextConversionTo.setText("")
         binding.editTextConversionFrom.setText("")
     }
-    private fun buildSpinner() {
-        val spinnerFrom: Spinner = findViewById(R.id.spinnerConversionFrom)
-        val spinnerTo: Spinner = findViewById(R.id.spinnerConversionTo)
+    private fun disableButton(button: Button) {
+        button?.isEnabled = false
+        button?.setTextColor(ContextCompat.getColor(button.context, R.color.black))
+        button?.setBackgroundColor(ContextCompat.getColor(button.context, R.color.teal_200))
+    }
+    private fun enableButton(button: Button) {
+        button?.isEnabled = true
+        button?.setTextColor(ContextCompat.getColor(button.context, R.color.white))
+        button?.setBackgroundColor(ContextCompat.getColor(button.context, R.color.purple_700))
+    }
+
+    private fun buildSpinner(view: View) {
+        val spinnerFrom: Spinner = binding.spinnerConversionFrom
+        //val spinnerTo: Spinner = findViewById(R.id.spinnerConversionTo)
+        val spinnerTo: Spinner = binding.spinnerConversionTo
         //creates spinners and populates them with arrays stored in strings.xml page
         ArrayAdapter.createFromResource(this, R.array.CurrenciesOne, android.R.layout.simple_spinner_item).also { adapter ->
             adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
