ee61f7511dcea2780cd5ad20c6a61838f0753edd
Devrock2847 <93879277+Devrock2847@users.noreply.github.com>
2023-06-29 00:40:31 +0100

    Fixed thread issue so repeat calls can be made

--- app/src/main/java/ucm/co5225/j96000/MainActivity.kt
+++ app/src/main/java/ucm/co5225/j96000/MainActivity.kt
@@ -51,9 +51,6 @@ class MainActivity: AppCompatActivity(), View.OnClickListener {
     private fun getApiResult(view: View) {
         disableButton(buttonConvert)
         val thread = Thread {
-        //this@MainActivity.runOnUiThread(java.lang.Runnable {
-
-            //var API = "https://v6.exchangerate-api.com/v6/$apiKey/pair/$baseCurrencyOne/$baseCurrencyTwo"
             if (binding.editTextConversionFrom != null && binding.editTextConversionFrom.text.isNotEmpty() && binding.editTextConversionFrom.text.isNotBlank()) {
                 //This stops the program crashing when no value is selected
                 if (baseCurrencyOne == baseCurrencyTwo) {
@@ -62,45 +59,39 @@ class MainActivity: AppCompatActivity(), View.OnClickListener {
                     }
                     //binding.textViewWarning.setText("Please select a currency to convert")
                 } else {
-                        //attempts to read from the api
-                        val apiResult = URL(api).readText()
-                        val jsonObject = JSONObject(apiResult)
-                        //pulls the conversion rate from the api call object
-                        conversionRateOne =
-                            jsonObject.getJSONObject("rates").getJSONObject("$baseCurrencyOne")
-                                .getString("value").toFloat()
-                        conversionRateTwo =
-                            jsonObject.getJSONObject("rates").getJSONObject("$baseCurrencyTwo")
-                                .getString("value").toFloat()
-                        conversionRate = conversionRateOne / conversionRateTwo * 100
+                    //attempts to read from the api
+                    val apiResult = URL(api).readText()
+                    val jsonObject = JSONObject(apiResult)
+                    //pulls the conversion rate from the api call object
+                    conversionRateOne =
+                        jsonObject.getJSONObject("rates").getJSONObject("$baseCurrencyOne")
+                            .getString("value").toFloat()
+                    conversionRateTwo =
+                        jsonObject.getJSONObject("rates").getJSONObject("$baseCurrencyTwo")
+                            .getString("value").toFloat()
+                    conversionRate = conversionRateOne / conversionRateTwo * 10
+                    //Log.d("Main", "$conversionRateOne")
+                    //Log.d("Main", "$conversionRateTwo")
+                    //Log.d("Main", "$conversionRate")
+                    //Log.d("Main", apiResult)
+                    //this Coroutine takes the data from the input boss and multiplies it
+                    //by the conversion rate and displays inside the output box
+                    runOnUiThread() {
+                        if (binding.textViewWarning.text.isNotBlank() || binding.textViewWarning.text.isNotEmpty()) {
+                            clearTextView(binding.textViewWarning)
+                        } else {
+                            val text = ((binding.editTextConversionFrom.text.toString()
+                                .toFloat()) * conversionRate).toString()
+                            binding.editTextConversionTo?.setText(text)
 
-                        //Log.d("Main", "$conversionRateOne")
-                        //Log.d("Main", "$conversionRateTwo")
-                        //Log.d("Main", "$conversionRate")
-                        //Log.d("Main", apiResult)
-                        //this Coroutine takes the data from the input boss and multiplies it
-                        //by the conversion rate and displays inside the output box
-                        runOnUiThread() {
-                            if (binding.textViewWarning.text.isNotBlank() || binding.textViewWarning.text.isNotEmpty()) {
-                                clearTextView(binding.textViewWarning)
-                            } else {
-                                val text = ((binding.editTextConversionFrom.text.toString()
-                                    .toFloat()) * conversionRate).toString()
-                                binding.editTextConversionTo?.setText(text)
-
-                                if (conversionRate != null) {
-                                    binding.textView2.text = conversionRate.toString()
-                                }
+                            if (conversionRate != null) {
+                                binding.textView2.text = conversionRate.toString()
                             }
                         }
-                            //Thread.sleep(500)
-                        //} catch (e: Exception) {
-                            //Add API fail notification to user
-                            //Log.e("Main", "$e")
-                        //}
                     }
                 }
             }
+        }
         thread.start()
         Thread.sleep(500)
         enableButton(buttonConvert)
